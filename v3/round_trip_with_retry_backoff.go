d782d8b73a3eae5defca54f03b701f97f1016b18
Mars Hall <mars@heroku.com>
2018-10-24 15:38:18 -0700

    Tune retry-with-backoff timing for Heroku API rate limiting

--- v3/round_trip_with_retry_backoff.go
+++ v3/round_trip_with_retry_backoff.go
@@ -3,13 +3,14 @@ package heroku
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/cenkalti/backoff"
 )
 
 // net/http RoundTripper interface, a.k.a. Transport
 // https://godoc.org/net/http#RoundTripper
-type RoundTripWithRetryBackoff struct {}
+type RoundTripWithRetryBackoff struct{}
 
 func (_ RoundTripWithRetryBackoff) RoundTrip(req *http.Request) (*http.Response, error) {
 	var lastResponse *http.Response
@@ -25,7 +26,19 @@ func (_ RoundTripWithRetryBackoff) RoundTrip(req *http.Request) (*http.Response,
 		return nil
 	}
 
-	err := backoff.Retry(retryableRoundTrip, backoff.NewExponentialBackOff())
+	rateLimitRetryConfig := &backoff.ExponentialBackOff{
+		Clock:               backoff.SystemClock,
+		InitialInterval:     30 * time.Second,
+		RandomizationFactor: 0.25,
+		Multiplier:          2,
+		MaxInterval:         15 * time.Minute,
+		// After MaxElapsedTime the ExponentialBackOff stops.
+		// It never stops if MaxElapsedTime == 0.
+		MaxElapsedTime: 0,
+	}
+	rateLimitRetryConfig.Reset()
+
+	err := backoff.Retry(retryableRoundTrip, rateLimitRetryConfig)
 	// Propagate the rate limit error when retries eventually fail.
 	if err != nil {
 		if lastResponse != nil {
